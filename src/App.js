3a57f23b8c797f7fa63f84ade71ab3001476179b
sdonich <sdonich@gmail.com>
2019-09-16 21:37:45 +0200

    Dirty commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div data-test="component-app" />
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr } from '../test/testUtils';
+import App from './App';
+
+const setup = () => {
+  return shallow(<App />);
+}
+
+it('App renders without error', () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, 'component-app');
+  expect(component.length).toBe(1);
+});
