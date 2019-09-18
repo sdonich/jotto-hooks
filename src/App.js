58a50d546c8c14f51f94d1d3ad470a4ebd19e0db
Sergei <sdonich@gmail.com>
2019-09-18 15:00:19 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import hookActions from './actions/hookActions';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'setSecretWord':
+      return { ...state, secretWord: action.payload }
+
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+}
 
 function App() {
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    { secretWord: null }
+  );
+  const setSecretWord = secretWord => dispatch({ type: 'setSecretWord', payload: secretWord });
+  React.useEffect(() => hookActions.getSecretWord(setSecretWord), []);
   return (
     <div data-test="component-app" />
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
 
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  return mount(<App />);
 }
 
-it('App renders without error', () => {
+test('App renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
+});
+
+describe('getSecretWord calls', () => {
+  test('getSecretWord gets called on App mount', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
+  // test('', () => {
+
+  // });
+
 });
